5e32bd14b12b9273ea7e9ae5a8def806cf7a06f1
tomSeng <tom.seng@epitech.eu>
2024-01-30 23:48:54 +0100

    [FIX] fixed checking errors in my_strcmp() function

--- JSONreader/lib/my_strcmp.c
+++ JSONreader/lib/my_strcmp.c
@@ -9,9 +9,13 @@
 
 int my_strcmp(const char *str, const char *str2)
 {
-    int str1_len = my_strlen(str);
-    int str2_len = my_strlen(str2);
+    int str1_len = 0;
+    int str2_len = 0;
 
+    if (str == NULL || str2 == NULL)
+        return -1;
+    str1_len = my_strlen(str);
+    str2_len = my_strlen(str2);
     if (str1_len != str2_len)
         return str1_len - str2_len;
     for (int i = 0; str[i] != '\0' && str2[i] != '\0'; i += 1) {
