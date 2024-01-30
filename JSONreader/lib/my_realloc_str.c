b082a1d4aa6189a2d2a23093d226e328ee4c6a9e
tomSeng <tom.seng@epitech.eu>
2024-01-30 23:42:15 +0100

    [ADD] my_realloc_str() function that realloc an str

---
+++ JSONreader/lib/my_realloc_str.c
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2024
+** anta-website
+** File description:
+** my_realloc_str
+*/
+
+#include "../includes/JSON_lib.h"
+
+int my_realloc_str(char **str, int reallocated_memory)
+{
+    char *old_str = (*str);
+
+    if (str == NULL)
+        return -1;
+    if (reallocated_memory == 0) {
+        free((*str));
+        (*str) = NULL;
+        return 0;
+    }
+    (*str) = malloc(sizeof(char) * reallocated_memory);
+    for (int i = 0; i < my_strlen(old_str) && i < reallocated_memory; i += 1)
+        (*str)[i] = old_str[i];
+    free(old_str);
+    return 0;
+}
