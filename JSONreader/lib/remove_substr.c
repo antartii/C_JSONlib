8460120540d06b27729803a4e21364d0607a5a8a
tomSeng <tom.seng@epitech.eu>
2024-01-30 23:54:13 +0100

    [ADD] added remove_substr() function that remove a part of an str based on it's index

---
+++ JSONreader/lib/remove_substr.c
@@ -0,0 +1,56 @@
+/*
+** LIB_JSON, 2024
+** remove_substr.c
+** File description:
+** Remove a part of an str.
+*/
+
+#include "../includes/JSON_lib.h"
+
+static int errors_remove_substr(char **adress, int start_index, int end_index)
+{
+    int len_str = 0;
+
+    if ((*adress) == NULL)
+        return -101;
+    len_str = my_strlen((*adress));
+    if (len_str == 0)
+        return -100;
+    if (end_index > len_str - 1)
+        return -102;
+    if (start_index < 0)
+        return -104;
+    if (end_index < 0)
+        return -105;
+    if (start_index > end_index)
+        return -103;
+    return 0;
+}
+
+int remove_substr(char **str_adress, int start_index, int end_index)
+{
+    int len_str = 0;
+    int new_len = 0;
+    char *temp = (*str_adress);
+    int index = 0;
+    int error = errors_remove_substr(str_adress, start_index, end_index);
+
+    if (error != 0)
+        return error;
+    len_str = my_strlen((*str_adress));
+    new_len = len_str - (end_index - start_index);
+    if (start_index == 0 && end_index == len_str - 1) {
+        free((*str_adress));
+        (*str_adress) = NULL;
+        return 0;
+    }
+    for (int i = 0; temp[i] != '\0'; i += 1) {
+        if (i < start_index || i > end_index) {
+            (*str_adress)[index] = temp[i];
+            index += 1;
+        }
+    }
+    (*str_adress)[index] = '\0';
+    (*str_adress) = realloc((*str_adress), sizeof(char) * new_len);
+    return 0;
+}
