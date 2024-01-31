74d38853eb9ad9abffec3b89f98bc1a0cf441189
tomSeng <tom.seng@epitech.eu>
2024-01-31 09:19:24 +0100

    [ADD] added JSON_get_from_keys() function that search with multiple layers of keys a value inside of a JSON

---
+++ JSONreader/src/JSON/GET/JSON_get_from_keys.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2024
+** anta-website
+** File description:
+** JSON_get_from_keys
+*/
+
+#include "../../../includes/JSON_lib.h"
+
+char *JSON_get_from_keys(char *JSON, int count, ...)
+{
+    char *JSONvalue = JSON;
+    char *temp = NULL;
+    va_list keys;
+    va_start(keys, count);
+
+    for (int i = 0; i < count; i += 1) {
+        JSONvalue = JSON_get_from_key(JSONvalue, va_arg(keys, char *));
+        if (temp != NULL) {
+            free(temp);
+        }
+        temp = JSONvalue;
+    }
+    return JSONvalue;
+}
