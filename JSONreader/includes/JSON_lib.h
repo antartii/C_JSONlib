f2267029b9bb613e6f21892a96a8eef9bd7d9b98
tomSeng <tom.seng@epitech.eu>
2024-01-31 08:49:22 +0100

    [ADD] .h of the base lib of JSON

---
+++ JSONreader/includes/JSON_lib.h
@@ -0,0 +1,28 @@
+/*
+** lib JSON, 2024
+** JSON_lib.h
+** File description:
+** lib of all functions from the JSON lib
+*/
+
+#ifndef JSON_LIB
+    #define JSON_LIB
+    #include <stddef.h>
+    #include <fcntl.h>
+    #include <sys/stat.h>
+    #include <unistd.h>
+    #include <stdarg.h>
+    #include <stdlib.h>
+
+// lib
+int my_strlen(const char *str);
+int my_strcmp(const char *str, const char *str2);
+int remove_substr(char **str_adress, int start_index, int end_index);
+char *get_file_content(char *file_path);
+char *select_str(const char *str, int start, int end);
+int insert_in_str(char **src, char *insert, int start_index);
+int my_realloc_str(char **str, int reallocated_memory);
+
+//
+
+#endif /* !JSON_LIB */
