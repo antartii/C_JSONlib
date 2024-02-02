04e5f06602bcf70356021c3359b602d0e7e4b0c1
tomSeng <tom.seng@epitech.eu>
2024-02-02 07:25:01 +0100

    [UPDATE] updated the JSON_clean() function to directly change the str instead of returning the changed str

--- CJSON/includes/CJSON.h
+++ CJSON/includes/CJSON.h
@@ -27,7 +27,7 @@ char *my_strdup(const char *src);
 // JSON lib
 char *JSON_get_from_key(char *JSON, char *key);
 char *JSON_get_from_keys(char *JSON, int count, ...);
-char *JSON_clean(char *JSON_string);
+int JSON_clean(char **JSON_string);
 char *JSON_ARRAY_get_from_index(char const *JSON_string, int index);
 //
 
--- CJSON/src/JSON/OTHER/JSON_clean.c
+++ CJSON/src/JSON/OTHER/JSON_clean.c
@@ -7,7 +7,7 @@
 
 #include "../../../includes/CJSON.h"
 
-char *JSON_clean(char *JSON_string)
+int JSON_clean(char **JSON_string)
 {
     int layer = 1;
     int reading_str = 0;
@@ -18,35 +18,36 @@ char *JSON_clean(char *JSON_string)
 
     //count char
 
-    for (int i = 0; JSON_string[i] != '\0'; i += 1) {
+    for (int i = 0; (*JSON_string)[i] != '\0'; i += 1) {
         if (line_index == 0) {
             for (int i = 0; i < layer - 1; i += 1) {
                 len_result += 1;
                 line_index += 1;
             }
         }
-        if (JSON_string[i] == '"') {
+        if ((*JSON_string)[i] == '"') {
             if (reading_str == 0)
                 reading_str = 1;
             else
                 reading_str = 0;
         }
-        if (JSON_string[i] != '\n' && JSON_string[i] != ' ' && JSON_string[i] != '\t' && reading_str == 0) {
-            len_result += 1;
-            line_index += 1;
-            if (JSON_string[i] == '{' || JSON_string[i] == '[') {
-                layer += 1;
-                line_index = 0;
+        if (reading_str == 0) {
+            if ((*JSON_string)[i] != '\n' && (*JSON_string)[i] != ' ' && (*JSON_string)[i] != '\t') {
                 len_result += 1;
-            } else if (JSON_string[i] == '}' || JSON_string[i] == ']') {
-                layer -= 1;
-                line_index = 0;
-                len_result += 1;
-                len_result += 1;
-            } else if (JSON_string[i] == ',') {
-                line_index = 0;
-                len_result += 1;
-            }       
+                line_index += 1;
+                if ((*JSON_string)[i] == '{' || (*JSON_string)[i] == '[') {
+                    layer += 1;
+                    line_index = 0;
+                    len_result += 1;
+                } else if ((*JSON_string)[i] == '}' || (*JSON_string)[i] == ']') {
+                    layer -= 1;
+                    line_index = 0;
+                    len_result += 2;
+                } else if ((*JSON_string)[i] == ',') {
+                    line_index = 0;
+                    len_result += 1;
+                }
+            }
         } else if (reading_str == 1)
             len_result += 1;
     }
@@ -59,42 +60,44 @@ char *JSON_clean(char *JSON_string)
     result_index = 0;
     line_index = 0;
 
-    for (int i = 0; JSON_string[i] != '\0'; i += 1) {
+    for (int i = 0; (*JSON_string)[i] != '\0'; i += 1) {
         if (line_index == 0) {
             for (int i = 0; i < layer - 1; i += 1) {
                 result[result_index ++] = '\t';
                 line_index += 1;
             }
         }
-        if (JSON_string[i] == '"') {
+        if ((*JSON_string)[i] == '"') {
             if (reading_str == 0)
                 reading_str = 1;
             else
                 reading_str = 0;
         }
-        if (JSON_string[i] != '\n' && JSON_string[i] != ' ' && JSON_string[i] != '\t' && reading_str == 0) {
-            result[result_index ++] = JSON_string[i];
+        if ((*JSON_string)[i] != '\n' && (*JSON_string)[i] != ' ' && (*JSON_string)[i] != '\t' && reading_str == 0) {
+            result[result_index ++] = (*JSON_string)[i];
             line_index += 1;
             if (reading_str == 0) {
-                if (JSON_string[i] == '{' || JSON_string[i] == '[') {
+                if ((*JSON_string)[i] == '{' || (*JSON_string)[i] == '[') {
                     layer += 1;
                     line_index = 0;
                     result[result_index++] = '\n';
-                } else if (JSON_string[i] == '}' || JSON_string[i] == ']') {
+                } else if ((*JSON_string)[i] == '}' || (*JSON_string)[i] == ']') {
                     layer -= 1;
                     result[result_index - 1] = '\n';
                     for (int i = 0; i < layer - 1; i += 1) {
                         result[result_index ++] = '\t';
                     }
-                    result[result_index ++] = JSON_string[i];
-                } else if (JSON_string[i] == ',') {
+                    result[result_index ++] = (*JSON_string)[i];
+                } else if ((*JSON_string)[i] == ',') {
                     line_index = 0;
                     result[result_index++] = '\n';
                 }
             }
         } else if (reading_str == 1)
-            result[result_index++] = JSON_string[i];
+            result[result_index++] = (*JSON_string)[i];
     }
     result[result_index ++] = '\0';
-    return result;
+    free((*JSON_string));
+    (*JSON_string) = result;
+    return 0;
 }
